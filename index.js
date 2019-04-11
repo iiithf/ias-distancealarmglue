6c74be0c0163f2c2ac727356be5e3a909868e8d4
Subhajit Sahu <wolfram77@gmail.com>
2019-04-11 19:07:27 +0530

    :mushroom: use env SOURCE, TARGET, DATARATE

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const needle = require('needle');
 
 
 const E = process.env;
-const SENSOR = E['SENSOR']||'';
-const ALARM = E['ALARM']||'';
+const SOURCE = E['SOURCE']||'';
+const TARGET = E['TARGET']||'';
 const DATARATE = parseInt(E['DATARATE']||'1000', 10);
 
 
 
 async function onInterval() {
-  if(!SENSOR) return;
-  var res = await needle('get', SENSOR);
-  console.log('SENSOR', SENSOR, res.body);
-  if(!ALARM) return;
-  res = await needle('post', ALARM, res.body, {json: true});
-  console.log('ALARM', ALARM, res.body);
+  if(!SOURCE) return;
+  var res = await needle('get', SOURCE);
+  console.log('SOURCE', SOURCE, res.body);
+  if(!TARGET) return;
+  res = await needle('post', TARGET, res.body, {json: true});
+  console.log('TARGET', TARGET, res.body);
 }
 console.log('DISTANCEALARMGLUE running');
 setInterval(onInterval, DATARATE);
